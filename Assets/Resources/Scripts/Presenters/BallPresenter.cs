79b71e136814cac2c8f4e555ff484a3e99a592c0
Hsantos <igor.hos@gmail.com>
2022-03-03 12:35:02 -0300

    Add rocket sprite and create bubble particles for ball movement

--- Assets/Resources/Scripts/Presenters/BallPresenter.cs
+++ Assets/Resources/Scripts/Presenters/BallPresenter.cs
@@ -6,6 +6,8 @@ namespace hSantos.BattleTank.Presenters {
     [RequireComponent(typeof(Rigidbody2D))]
     public class BallPresenter : MonoBehaviour, IBallOutput {
         private Rigidbody2D body;
+
+        [SerializeField] private ParticleSystem particleSystem;
         public Vector2 Velocity => body.velocity;
         private void Awake() {
             body = GetComponent<Rigidbody2D>();
@@ -13,6 +15,12 @@ namespace hSantos.BattleTank.Presenters {
 
         public void ReceiveHit(Vector2 hit) {
             body.AddForce(hit, ForceMode2D.Impulse);
+
+            var particleSystemVelocityOverLifetime = particleSystem.velocityOverLifetime;
+            particleSystemVelocityOverLifetime.yMultiplier = -(Mathf.Round(hit.normalized.y));
+
+            
+            particleSystem.Play();
         }
 
         public void ReceiveFriction(Vector2 friction) {
